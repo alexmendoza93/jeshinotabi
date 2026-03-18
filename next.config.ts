80d1b055274c3c1901f7953af79c7399f0e5b317
alexmendoza93 <tiburon9386@gmail.com>
2026-03-18 11:17:27 -0600

    next config github pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
