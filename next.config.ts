943f641e66400ce5df27f38e9265e718f9fa7eb7
alexmendoza93 <tiburon9386@gmail.com>
2026-04-14 15:31:17 -0600

    eliminate jeshinotabi as basePath in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/jeshinotabi" : "";
+// const isProd = process.env.NODE_ENV === "production";
+const basePath = "";
 
 const nextConfig: NextConfig = {
   output: "export",
